b9201d0fd9bcc6422184088063c0700343dd91f7
sahu-suraj <70439626+sahu-suraj@users.noreply.github.com>
2026-05-08 12:30:34 +0530

    Clear user and accessKey in lambdatest-setup.js
    Updated user and accessKey fields to be empty for security.

--- lambdatest-setup.js
+++ lambdatest-setup.js
@@ -10,8 +10,8 @@ const capabilities = {
     'platformName': process.env.HYPEREXECUTE_PLATFORM || 'Windows 11', 
     'build': 'Playwright HyperExecute Build',
     'name': 'Playwright HyperExecute Test',
-    'user': 'sahusuraj4292',
-    'accessKey': 'sLzh3dkbNs7zLOzrcprQoa86Ui9plCa2V421ZNVOJbIfyoOHdm',
+    'user': '',//Add TestMUAi username
+    'accessKey': '',// Add Access Key
     'network': true,
     'video': true,
     'console': true
